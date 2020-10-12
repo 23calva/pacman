417d024aaa3dbe6af55ea2cf41594d587dbc41aa
23connd <70708424+23connd@users.noreply.github.com>
2020-10-12 10:14:04 -0400

    death
    die when hit

---
+++ Assets/Scripts/DestroyPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+}
