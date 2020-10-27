a32a2609d88f491138ed6b810a5dfd703eb89b8c
23hitth <23hitth@live.elida.k12.oh.us>
2020-10-27 10:45:55 -0400

    1

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelletIgnore : MonoBehaviour
+{
+    private object Enemy;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Physics.IgnoreCollision(Enemy.collider, Collider);
+        }
+    }
+}
